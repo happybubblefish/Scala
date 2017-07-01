59f0ca48037a88cc7981fb922126628599d98e64
bubblefish <congls2007@yahoo.com>
2017-07-01 14:14:30 -0400

    First commit

---
+++ LearningScala/src/com/lin/scala/basic/StringDemo.scala
@@ -0,0 +1,15 @@
+package com.lin.scala.basic
+
+/**
+  * Created by congl on 7/1/2017.
+  */
+object PrimitiveDemo {
+  def main(args: Array[String]): Unit ={
+    val msg = "Hello from Scala";
+    println(msg)
+
+//    msg = "val variable cannot be assigned to another varaible";
+
+    println("Haha")
+  }
+}
